ca3001043b1678a70ea9ebe93d0138d56989d52c
Jorge Arias <94802496+joariasdev@users.noreply.github.com>
2026-03-21 13:28:58 -0400

    Add board service

--- src/Kanban.API/Controllers/BoardsController.cs
+++ src/Kanban.API/Controllers/BoardsController.cs
@@ -1,6 +1,6 @@
-﻿using Kanban.API.Models.DTOs;
-using Kanban.Domain.Entities;
-using Kanban.Infrastructure.Repositories;
+﻿using Kanban.Application.DTOs;
+using Kanban.Application.Responses;
+using Kanban.Application.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kanban.API.Controllers;
@@ -9,94 +9,40 @@ namespace Kanban.API.Controllers;
 [Route("api/[controller]")]
 public class BoardsController : ControllerBase
 {
-    private readonly BoardRepository _boardRepository;
-    public BoardsController(BoardRepository boardRepository)
+    private readonly BoardService _boardService;
+    public BoardsController(BoardService boardService)
     {
-        _boardRepository = boardRepository;
+        _boardService = boardService;
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<BoardDTO>> GetAll()
+    public ApiResponse<List<BoardDTO>> GetAll()
     {
-        var boardDTOs = _boardRepository.GetAll().Select(b => new BoardDTO { Id = b.Id, Name = b.Name }).ToList();
-        return Ok(boardDTOs);
+        return _boardService.GetAll();
+        
     }
 
     [HttpGet("{id}")]
-    public ActionResult<BoardDTO> GetById(int id)
+    public ApiResponse<BoardDTO> GetById(int id)
     {
-        var board = _boardRepository.GetById(id);
-
-        if (board == null)
-        {
-            return NotFound();
-        }
-
-        var boardDTO = new BoardDTO { Id = board.Id, Name = board.Name };
-
-        return Ok(boardDTO);
+        return _boardService.GetById(id);
     }
 
     [HttpPost]
-    public ActionResult<BoardDTO> Create(CreateBoardDTO boardRequest)
+    public ApiResponse<BoardDTO> Create(CreateBoardDTO boardRequest)
     {
-        if (string.IsNullOrWhiteSpace(boardRequest.Name))
-        {
-            return BadRequest("Name is required.");
-        }
-
-        var board = new Board
-        {
-            Name = boardRequest.Name
-        };
-
-        _boardRepository.Create(board);
-
-        var boardDTO = new BoardDTO
-        {
-            Id = board.Id,
-            Name = board.Name
-        };
-
-        return CreatedAtAction(nameof(GetById), new { id = board.Id }, boardDTO);
+        return _boardService.Create(boardRequest);
     }
 
     [HttpPut("{id}")]
-    public IActionResult Update(int id, BoardDTO board)
+    public ApiResponse<BoardDTO> Update(int id, BoardDTO board)
     {
-        if (id != board.Id)
-        {
-            return BadRequest("Incorrect Id");
-        }
-
-        if (string.IsNullOrWhiteSpace(board.Name))
-        {
-            return BadRequest("Name is required");
-        }
-
-        var existingBoard = _boardRepository.GetById(id);
-
-        if (existingBoard == null)
-        {
-            return NotFound();
-        }
-
-        existingBoard.Name = board.Name;
-
-        _boardRepository.Update(existingBoard);
-        
-        return NoContent();
+       return _boardService.Update(id, board);
     }
 
     [HttpDelete("{id}")]
-    public IActionResult Delete(int id)
+    public ApiResponse<BoardDTO>  Delete(int id)
     {
-        var board = _boardRepository.GetById(id);
-        if (board == null)
-        {
-            return NotFound();
-        }
-        _boardRepository.Delete(id);
-        return NoContent();
+       return _boardService.Delete(id);
     }
 }
---
+++ src/Kanban.Application/Services/BoardService.cs
@@ -0,0 +1,97 @@
+﻿
+using Kanban.Application.DTOs;
+using Kanban.Application.Responses;
+using Kanban.Domain.Entities;
+using Kanban.Infrastructure.Repositories;
+
+namespace Kanban.Application.Services;
+
+public class BoardService
+{
+    private readonly BoardRepository _boardRepository;
+    public BoardService(BoardRepository boardRepository)
+    {
+        _boardRepository = boardRepository;
+    }
+    public ApiResponse<List<BoardDTO>> GetAll()
+    {
+        var boardDTOs = _boardRepository.GetAll().Select(b => new BoardDTO { Id = b.Id, Name = b.Name }).ToList();
+        return ApiResponse<List<BoardDTO>>.SuccessResponse(boardDTOs);
+    }
+
+   
+    public ApiResponse<BoardDTO> GetById(int id)
+    {
+        var board = _boardRepository.GetById(id);
+
+        if (board == null)
+        {
+            return ApiResponse<BoardDTO>.FailureResponse($"Board with {id} not found", 404);
+        }
+
+        var boardDTO = new BoardDTO { Id = board.Id, Name = board.Name };
+
+        return ApiResponse<BoardDTO>.SuccessResponse(boardDTO);
+    }
+
+   
+    public ApiResponse<BoardDTO> Create(CreateBoardDTO boardRequest)
+    {
+        if (string.IsNullOrWhiteSpace(boardRequest.Name))
+        {
+            return ApiResponse<BoardDTO>.FailureResponse("Name is required");
+        }
+
+        var board = new Board
+        {
+            Name = boardRequest.Name
+        };
+
+        _boardRepository.Create(board);
+
+        var boardDTO = new BoardDTO
+        {
+            Id = board.Id,
+            Name = board.Name
+        };
+
+        return ApiResponse<BoardDTO>.SuccessResponse(boardDTO);
+    }
+
+    public ApiResponse<BoardDTO> Update(int id, BoardDTO board)
+    {
+        if (id != board.Id)
+        {
+            return ApiResponse<BoardDTO>.FailureResponse("Incorrect Id");
+        }
+
+        if (string.IsNullOrWhiteSpace(board.Name))
+        {
+           return ApiResponse<BoardDTO>.FailureResponse("Name is required");
+        }
+
+        var existingBoard = _boardRepository.GetById(id);
+
+        if (existingBoard == null)
+        {
+            return ApiResponse<BoardDTO>.FailureResponse($"Board with {id} not found", 404);
+        }
+
+        existingBoard.Name = board.Name;
+
+        _boardRepository.Update(existingBoard);
+        
+        return ApiResponse<BoardDTO>.SuccessResponse(null);
+    }
+   
+    public ApiResponse<BoardDTO> Delete(int id)
+    {
+        var board = _boardRepository.GetById(id);
+        if (board == null)
+        {
+            return ApiResponse<BoardDTO>.FailureResponse($"Board with {id} not found", 404);
+        }
+        _boardRepository.Delete(id);
+        return ApiResponse<BoardDTO>.SuccessResponse(null);
+    }
+}
